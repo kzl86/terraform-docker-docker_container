dc81dd887dab2cdd89303ee2c31d96812443e7f8
Zoltan L. Kiss <laszlo.zoltan.kiss@gmail.com>
2024-03-26 16:06:44 +0100

    Update terraform and docker provider version

--- main.tf
+++ main.tf
@@ -1,79 +1,79 @@
 locals {
-    mounts = var.mounts != null ? { 
-        for index, mount in var.mounts :
-            index => {
-                "target"    = lookup(mount, "target", null)
-                "type"      = lookup(mount, "type", null)
-                "source"    = lookup(mount, "source", null)
-                "read_only" = lookup(mount, "read_only", null)
-            }
-    } : {}
+  mounts = var.mounts != null ? {
+    for index, mount in var.mounts :
+    index => {
+      "target"    = lookup(mount, "target", null)
+      "type"      = lookup(mount, "type", null)
+      "source"    = lookup(mount, "source", null)
+      "read_only" = lookup(mount, "read_only", null)
+    }
+  } : {}
 
-    ports = var.ports != null ? { 
-        for index, port in var.ports :
-            index => {
-                "internal" = lookup(port, "internal", null)
-                "external" = lookup(port, "external", null)
-                "protocol" = lookup(port, "protocol", null)
-            }
-    } : {}
+  ports = var.ports != null ? {
+    for index, port in var.ports :
+    index => {
+      "internal" = lookup(port, "internal", null)
+      "external" = lookup(port, "external", null)
+      "protocol" = lookup(port, "protocol", null)
+    }
+  } : {}
 
-    networks_advanced = var.networks_advanced != null ? { 
-        for index, network in var.networks_advanced :
-            index => {
-                "name"    = lookup(network, "name", null)
-                "aliases" = lookup(network, "aliases", null)
-            }
-    } : {}
+  networks_advanced = var.networks_advanced != null ? {
+    for index, network in var.networks_advanced :
+    index => {
+      "name"    = lookup(network, "name", null)
+      "aliases" = lookup(network, "aliases", null)
+    }
+  } : {}
 }
 
 resource "docker_container" "this" {
-    name         = var.name
-    image        = var.image
-    restart      = var.restart
-    network_mode = var.network_mode
+  name         = var.name
+  image        = var.image
+  restart      = var.restart
+  network_mode = var.network_mode
 
-    env = var.env
+  env = var.env
 
-    dynamic "labels" {
-        for_each = var.labels
+  dynamic "labels" {
+    for_each = var.labels
 
-        content {
-            label = labels.key
-            value = labels.value
-        }
+    content {
+      label = labels.key
+      value = labels.value
     }
+  }
 
-    dynamic "mounts" {
-        for_each = local.mounts
+  dynamic "mounts" {
+    for_each = local.mounts
 
-        content {
-            target    = lookup(mounts.value, "target", null)
-            type      = lookup(mounts.value, "type", null)
-            source    = lookup(mounts.value, "source", null)
-            read_only = lookup(mounts.value, "read_only", false)
-        }
+    content {
+      target    = lookup(mounts.value, "target", null)
+      type      = lookup(mounts.value, "type", null)
+      source    = lookup(mounts.value, "source", null)
+      read_only = lookup(mounts.value, "read_only", false)
     }
+  }
 
-    dynamic "ports" {
-        for_each = local.ports
+  dynamic "ports" {
+    for_each = local.ports
 
-        content {
-            internal = lookup(ports.value, "internal", null)
-            external = lookup(ports.value, "external", null)
-            protocol = lookup(ports.value, "protocol", null)
-        }
+    content {
+      internal = lookup(ports.value, "internal", null)
+      external = lookup(ports.value, "external", null)
+      protocol = lookup(ports.value, "protocol", null)
     }
+  }
 
-    dynamic "networks_advanced" {
-        for_each = local.networks_advanced
+  dynamic "networks_advanced" {
+    for_each = local.networks_advanced
 
-        content {
-            name    = lookup(networks_advanced.value, "name", null)
-            aliases = lookup(networks_advanced.value, "aliases", null)
-        }
+    content {
+      name    = lookup(networks_advanced.value, "name", null)
+      aliases = lookup(networks_advanced.value, "aliases", null)
     }
+  }
 
-    entrypoint = var.entrypoint
+  entrypoint = var.entrypoint
 
 }
--- variables.tf
+++ variables.tf
@@ -1,57 +1,57 @@
 variable "name" {
-    description = "The name of the container."
-    type        = string
+  description = "The name of the container."
+  type        = string
 }
 
 variable "image" {
-    description = "The ID of the image to back this container. The easiest way to get this value is to use the docker_image resource as is shown in the example."
-    type        = string
+  description = "The ID of the image to back this container. The easiest way to get this value is to use the docker_image resource as is shown in the example."
+  type        = string
 }
 
 variable "restart" {
-    description = "The restart policy for the container. Must be one of 'no', 'on-failure', 'always', 'unless-stopped'. Defaults to no."
-    type        = string
-    default     = "no"
+  description = "The restart policy for the container. Must be one of 'no', 'on-failure', 'always', 'unless-stopped'. Defaults to no."
+  type        = string
+  default     = "no"
 }
 
 variable "labels" {
-    description = "User-defined key/value metadata"
-    type        = map
-    default     = {}
+  description = "User-defined key/value metadata"
+  type        = map(any)
+  default     = {}
 }
 
 variable "env" {
-    description = "Environment variables to set in the form of KEY=VALUE, e.g. DEBUG=0"
-    type        = list(string)
-    default     = []
+  description = "Environment variables to set in the form of KEY=VALUE, e.g. DEBUG=0"
+  type        = list(string)
+  default     = []
 }
 
 variable "mounts" {
-    description = "Specification for mounts to be added to containers created as part of the service."
-    type        = list
-    default     = null
+  description = "Specification for mounts to be added to containers created as part of the service."
+  type        = list(any)
+  default     = null
 }
 
 variable "ports" {
-    description = "Publish a container's port(s) to the host."
-    type        = list
-    default     = null
+  description = "Publish a container's port(s) to the host."
+  type        = list(any)
+  default     = null
 }
 
 variable "network_mode" {
-    description = "Network mode of the container."
-    type        = string
-    default     = "default"
+  description = "Network mode of the container."
+  type        = string
+  default     = "default"
 }
 
 variable "networks_advanced" {
-    description = "The networks the container is attached to."
-    type        = list
-    default     = null
+  description = "The networks the container is attached to."
+  type        = list(any)
+  default     = null
 }
 
 variable "entrypoint" {
-    description = "The command to use as the Entrypoint for the container. The Entrypoint allows you to configure a container to run as an executable."
-    type        = list(string)
-    default     = []
+  description = "The command to use as the Entrypoint for the container. The Entrypoint allows you to configure a container to run as an executable."
+  type        = list(string)
+  default     = []
 }
